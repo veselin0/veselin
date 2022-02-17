de0d1f54dc6bc1486c4d7768caf3473909ffdbf3
Veselin <vesselin60@gmail.com>
2022-02-17 10:44:37 +0200

    Change NavLink for Link

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,40 +1,33 @@
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Home = () => {
 	return (
 		<div className="container">
 			<div>
 				<h2>About</h2>
-				<NavLink to="/about">
+				<Link to="/about">
                     <img
                         src={require("../imgs/me.jpg")}
                         alt="me"
                         width="250px"
                         height="300px"
                     />
-				</NavLink>
+				</Link>
 			</div>
 			<div>
 				<h2>Portfolio</h2>
-				<NavLink to="/portfolio">
+				<Link to="/portfolio">
                     <img
                         src={require("../imgs/hired.PNG")}
                         alt="hired"
                         width="400"
                         highth="600"
                     />
-				</NavLink>
+				</Link>
 			</div>
 			<div>
-				<h2>Contact</h2>
-				<NavLink to="/contact">
-                    <img
-                        src={require("../imgs/duff.JPG")}
-                        alt="hired"
-                        width="300px"
-                        height="250px"
-                    />
-				</NavLink>
+				<h2>Language</h2>
+				
 			</div>
 		</div>
 	);
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,49 +1,49 @@
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Portfolio = () => {
 	return (
-		<div className="about-container">
+		<div className="portfolio-container">
 			<div className="home-link">
 				<h2>Home</h2>
-				<NavLink to="/">
-                    <img
-                        src={require("../imgs/me.jpg")}
-                        alt="me"
-                        width="250px"
-                        height="300px"
-                    />
-				</NavLink>
+				<Link to="/">
+					<img
+						src={require("../imgs/me.jpg")}
+						alt="me"
+						width="250px"
+						height="300px"
+					/>
+				</Link>
 			</div>
-			<main className="about-main">
+			<main className="portfolio-main">
 				<h1>A Pinch of Projects</h1>
 				<h2>My Dashboard</h2>
 				<p>A useful example of a dashboard written in JavaScript</p>
-                <a
-                    href="https://veselin0.github.io/My-Dashboard/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <img
-                        src={require("../imgs/my-dashboard.png")}
-                        alt="me"
-                        width="500px"
-                        height="300px"
-                    />
-                </a>
-                <h2>Ecommerce Site</h2>
+				<a
+					href="https://veselin0.github.io/My-Dashboard/"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={require("../imgs/my-dashboard.png")}
+						alt="me"
+						width="500px"
+						height="300px"
+					/>
+				</a>
+				<h2>Ecommerce Site</h2>
 				<p>A minimalistic website written in Reactjs</p>
-                <a
-                    href="https://veselin0.github.io/Ecommerce-Site/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <img
-                        src={require("../imgs/ecommerce.png")}
-                        alt="me"
-                        width="500px"
-                        height="300px"
-                    />
-                </a>
+				<a
+					href="https://veselin0.github.io/Ecommerce-Site/"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={require("../imgs/ecommerce.png")}
+						alt="me"
+						width="500px"
+						height="300px"
+					/>
+				</a>
 			</main>
 		</div>
 	);
