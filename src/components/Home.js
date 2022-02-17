da6a8e77962232ba8b120918891a014e59ce88a9
Veselin <vesselin60@gmail.com>
2022-02-17 10:52:34 +0200

    code refactored

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,28 +6,27 @@ const Home = () => {
 			<div>
 				<h2>About</h2>
 				<Link to="/about">
-                    <img
-                        src={require("../imgs/me.jpg")}
-                        alt="me"
-                        width="250px"
-                        height="300px"
-                    />
+					<img
+						src={require("../imgs/me.jpg")}
+						alt="me"
+						width="250px"
+						height="300px"
+					/>
 				</Link>
 			</div>
 			<div>
 				<h2>Portfolio</h2>
 				<Link to="/portfolio">
-                    <img
-                        src={require("../imgs/hired.PNG")}
-                        alt="hired"
-                        width="400"
-                        highth="600"
-                    />
+					<img
+						src={require("../imgs/hired.PNG")}
+						alt="hired"
+						width="400"
+						highth="600"
+					/>
 				</Link>
 			</div>
 			<div>
 				<h2>Language</h2>
-				
 			</div>
 		</div>
 	);
